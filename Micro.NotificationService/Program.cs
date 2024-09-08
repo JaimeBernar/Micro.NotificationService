12c0da2b62a6f89f1b4d345cae3f041728693be5
Jaime <jbernar51@gmail.com>
2024-09-08 18:57:33 +0200

    log settings and add dockerfile and docker compose file

--- Micro.NotificationService/Program.cs
+++ Micro.NotificationService/Program.cs
@@ -5,8 +5,11 @@ namespace Micro.NotificationService
     using Microsoft.Extensions.Hosting;
     using Micro.NotificationService.Data;
     using Micro.NotificationService.Extensions;
+    using Microsoft.Extensions.Options;
+    using Micro.NotificationService.Options;
+    using Serilog;
 
-    public static class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
@@ -17,9 +20,30 @@ namespace Micro.NotificationService
 
             builder.Services.AddOptions(builder.Configuration);
             builder.Services.AddServices();
+            builder.Services.AddSerilog((context, configuration) =>
+            {
+                configuration.ReadFrom.Configuration(builder.Configuration);
+            });
 
             var app = builder.Build();
-            app.EnsureCreatedAndApplyMigrations();
+
+            var logger = app.Services.GetRequiredService<ILogger<Program>>();
+            var settings = app.Services.GetRequiredService<IOptions<SettingsOptions>>().Value;
+            var serverOptions = app.Services.GetRequiredService<IOptions<EmailServerOptions>>().Value;
+
+            logger.LogInformation("Logging Configuration");
+            logger.LogInformation("Settings:");
+            logger.LogInformation($"{nameof(SettingsOptions.EmailNotificationsActive)}={settings.EmailNotificationsActive}");
+            logger.LogInformation($"{nameof(SettingsOptions.WebNotificationsActive)}={settings.WebNotificationsActive}");
+            
+            logger.LogInformation("EmailServer:");
+            logger.LogInformation($"{nameof(EmailServerOptions.Host)}={serverOptions.Host}");
+            logger.LogInformation($"{nameof(EmailServerOptions.Port)}={serverOptions.Port}");
+            logger.LogInformation($"{nameof(EmailServerOptions.UseSenderInfo)}={serverOptions.UseSenderInfo}");
+            logger.LogInformation($"{nameof(EmailServerOptions.EmailSenderName)}={serverOptions.EmailSenderName}");
+            logger.LogInformation($"{nameof(EmailServerOptions.EmailSenderAddress)}={serverOptions.EmailSenderAddress}");
+
+            EnsureCreatedAndApplyMigrations(app);
             app.MapCarter();            
 
             // Configure the HTTP request pipeline.
@@ -34,7 +58,7 @@ namespace Micro.NotificationService
             app.Run();
         }
 
-        private static void EnsureCreatedAndApplyMigrations(this WebApplication app)
+        private static void EnsureCreatedAndApplyMigrations(WebApplication app)
         {
             using (var scope = app.Services.CreateScope())
             {
