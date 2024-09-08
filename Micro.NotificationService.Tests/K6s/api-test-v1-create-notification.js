c4925f14cd3062e01461699998308b7191936c58
Jaime <jbernar51@gmail.com>
2024-09-08 20:22:03 +0200

    Add first stress test

---
+++ Micro.NotificationService.Tests/K6s/api-test-v1-create-notification.js
@@ -0,0 +1,28 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '10s', target: 20 },  // Ramp-up to 20 users
+        { duration: '50s', target: 20 },  // Ramp-up to 20 users
+    ],
+};
+
+export default function () {
+    const apiUrl = 'http://localhost:8080/api/v1/notifications';
+
+    const request = {
+        notificationType: "Type",
+        channel: 0,
+        header: "Header",
+        body: "Body"
+    };
+
+    const response = http.post(apiUrl, JSON.stringify(request), {
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    check(response, {
+        'response code was 500': (res) => res.status == 500
+    });
+}
